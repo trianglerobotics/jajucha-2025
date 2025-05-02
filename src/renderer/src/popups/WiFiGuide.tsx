ae79e819b3d6b169b4459ac59d300a3645ac88f1
tr <first2021@kaist.ac.kr>
2025-05-02 18:37:06 +0900

    WIFI guid TYPO

--- src/renderer/src/popups/WiFiGuide.tsx
+++ src/renderer/src/popups/WiFiGuide.tsx
@@ -15,25 +15,24 @@ const WiFiGuide = ({ onClose }) => {
         </div>
         <div className='flex flex-col h-[500px] gap-8 overflow-auto mt-5 scrollbar-thin'>
           <p className="text-[16px] text-[#444444] font-medium">
-            1. PC USB 포트에 Wi-Fi dongle을 연결합니다. <br />
+            1. PC의 USB 포트에 Wi-Fi 동글을 연결합니다. <br />
           </p>
           <img src={DongleIMG} alt="Wi-Fi Icon" className="w-[300px]  mx-auto mb-4  " />
           <p className="text-[16px] text-[#444444] font-medium">
-            2. 아래 이미지를 클릭하여 어뎁터 설정창을 활성화 합니다 <br />
+            2. 아래 이미지를 클릭하여 어댑터 설정 창을 엽니다. <br />
           </p>        
           <img src={RunImg} onClick={window.api.openNcpa} alt="Wi-Fi Icon" className="w-[320px]  mx-auto mb-4  rounded-lg hover:cursor-pointer hover:scale-105 duration-300 hover:shadow-lg" />
           <p className="text-[16px] text-[#444444] font-medium">
-            3. TP-Link Wireless USB Adapter 에 해당하는 Wi-Fi 인터페이스 번호를 확인 합니다 <span className='font-base text-[14px] text-[#676767]' > 예시) Wi-Fi 2</span> 
+            3. TP-Link Wireless USB Adapter에 해당하는 Wi-Fi 인터페이스 이름(예: Wi-Fi 2)을 확인합니다. 
           </p>        
           <img src={AdapterImg} alt="Wi-Fi Icon" className="w-[320px]  mx-auto mb-4 " />
           <p className="text-[16px] text-[#444444] font-medium">
-            4. 이미지를 클릭하여 와이파이 연결창을 활성화 합니다
+            4. 아래 이미지를 클릭하여 Wi-Fi 연결 창을 엽니다.
             {/* <span className='font-bold text-[14px] text-[#676767]' > 보안 키는 'jajucha123'입니다.</span> */}
           </p>
           <img src={WiFiIMG} onClick={window.api.openWifiTray} alt="Wi-Fi Icon" className="w-[300px]  mx-auto mb-4 hover:cursor-pointer hover:scale-105 duration-300 hover:shadow-lg" />
           <p className="text-[16px] text-[#444444] font-medium">
-            5. Wi-Fi 인터페이스와 네트워크를 선택하고 보안 키를 입력 하세요
-            <span className='font-bold text-[14px] text-[#676767]' > 보안 키는 'jajucha123'입니다.</span>
+            5. 사용하려는 Wi-Fi 인터페이스와 네트워크를 선택한 후, 보안 키로 **jajucha123**을 입력하세요.
           </p>
           <img src={WiFiPWD}  alt="Wi-Fi Icon" className="w-[300px]  mx-auto mb-4" />
         </div>
